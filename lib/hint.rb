bef1d2ea8d49a5b99eea2d7dbc21abda4159d501
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-08-23 15:34:33 -0700

    Abstracted correct color&position hints out of #draw_hint_grid, into separate method, #get_black

--- lib/hint.rb
+++ lib/hint.rb
@@ -5,7 +5,10 @@ class Hint
     @correct_code
     @hint_colors = Colors.new.hint_colors
     @all_hints = []
+    @hint_display = []
   end
+
+
 
 #for each color in guess_code, find how many times it occurs
 #then how many times it occurs in correct_code
@@ -15,25 +18,30 @@ class Hint
   def draw_hint_grid(this_guess, correct_code)
     @guess_code = this_guess
     @correct_code = correct_code.split(",")
-    hint_display = []
     @correct_code.map!{|x| x.to_sym}
+    black_count = get_black()
     @correct_code.each_with_index do | color, index |
-      if(@guess_code[index] == @correct_code[index])
-        hint_display << @hint_colors[0]
-      elsif(@correct_code.include?(color) && @guess_code[index] != @correct_code[index])
-          hint_display << @hint_colors[1]
-
+      if(@correct_code.include?(color) && @guess_code[index] != @correct_code[index])
+          @hint_display << @hint_colors[1]
       else
-        hint_display << "_"
+        @hint_display << "_"
       end
     end
-    @all_hints << hint_display.join(",")
+    @all_hints << @hint_display.join(",")
     @all_hints.each do |line|
       puts line
     end
     spaces_filled = @all_hints.length
     (12 - spaces_filled).times do
       puts "_ _ _ _"
+    end
+  end
+
+  def get_black()
+    @correct_code.each_index do |index|
+      if(@guess_code[index] == @correct_code[index])
+        @hint_display << @hint_colors[0]
+      end
     end
   end
 end
